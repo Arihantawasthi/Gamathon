0f42d61ff225428538153a399f49722d94fd980a
Arihant Awasthi <awasthiarihant@gmail.com>
2020-01-21 22:43:53 +0530

    Added STATIC_ROOT in production settings

--- Gamathon/settings/prod.py
+++ Gamathon/settings/prod.py
@@ -3,3 +3,5 @@ from Gamathon.settings.base import *
 # Override base settings here for production environment.
 DEBUG = False
 ALLOWED_HOSTS = ['13.126.147.71']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
