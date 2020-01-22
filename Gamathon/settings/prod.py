e632f4e80ffd53ef0fcd72dbcdd81a755a51ca31
Arihant Awasthi <awasthiarihant@gmail.com>
2020-01-22 16:31:50 +0000

    Including ip in allowed hosts

--- Gamathon/settings/prod.py
+++ Gamathon/settings/prod.py
@@ -2,6 +2,6 @@ from Gamathon.settings.base import *
 
 # Override base settings here for production environment.
 DEBUG = False
-ALLOWED_HOSTS = ['13.126.115.12']
+ALLOWED_HOSTS = ['13.126.115.12', 'www.gamathon.gg', 'gamathon.gg']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
