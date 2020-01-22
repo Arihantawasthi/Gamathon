57fc416084390a0e807da484f3c0fe6e1c859f71
Arihant Awasthi <awasthiarihant@gmail.com>
2020-01-22 16:56:07 +0530

    Added right path for the images

--- Gamathon/settings/base.py
+++ Gamathon/settings/base.py
@@ -141,4 +141,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/Gamathon/media/'
+MEDIA_URL = '/media/'
--- Gamathon/settings/prod.py
+++ Gamathon/settings/prod.py
@@ -2,6 +2,6 @@ from Gamathon.settings.base import *
 
 # Override base settings here for production environment.
 DEBUG = False
-ALLOWED_HOSTS = ['13.126.115.12']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
