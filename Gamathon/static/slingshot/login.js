dd03fa225f387602e43d68d4cf310704a9a9802b
root <root@ip-172-31-40-4.ap-south-1.compute.internal>
2020-01-23 05:18:58 +0000

    modified for production

--- Gamathon/static/slingshot/login.js
+++ Gamathon/static/slingshot/login.js
@@ -23,7 +23,7 @@ $(document).on('submit','#login-form', function(e) {
             warning.innerHTML = response_data.message
         }
         else {
-            window.location.replace('http://13.126.115.12:8000')
+            window.location.replace('http://13.235.100.9')
         }
     })
 })
