ce128cfaffd9a3c3dd082b69d8321ad71a6f2598
Arihant Awasthi <awasthiarihant@gmail.com>
2020-02-02 15:06:10 +0530

    ip

--- Gamathon/settings/prod.py
+++ Gamathon/settings/prod.py
@@ -2,7 +2,7 @@ from Gamathon.settings.base import *
 
 # Override base settings here for production environment.
 DEBUG = False
-ALLOWED_HOSTS = ['gamathon.gg', 'www.gamathon.gg']
+ALLOWED_HOSTS = ['gamathon.gg', 'www.gamathon.gg', '127.0.0.1']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 INSTALLED_APPS.append('compressor')
