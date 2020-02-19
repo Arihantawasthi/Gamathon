1540d7544d423645fe5eb8f0b551cfefc363da60
root <root@ip-172-31-40-4.ap-south-1.compute.internal>
2020-02-19 21:37:46 +0000

    sldf

--- Gamathon/settings/prod.py
+++ Gamathon/settings/prod.py
@@ -1,7 +1,7 @@
 from Gamathon.settings.base import *
 
 # Override base settings here for production environment.
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['gamathon.gg', 'www.gamathon.gg']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
